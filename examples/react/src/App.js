04fb4c208163edb000587bebc34336e0ed0f586b
Roc Wu <rocwind@gmail.com>
2020-05-18 11:38:51 +0800

    chore: update example project with swr refresh() call

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -3,56 +3,71 @@ import React from 'react';
 import { createFetcher, CacheMode } from 'create-fetcher';
 import { useSWR, usePolling } from 'create-fetcher/lib/hooks';
 import { createLocalStorageCache } from 'create-fetcher/lib/caches/localStorage';
-import './App.css';
 
-const fetcher = createFetcher(
-    (_, { signal }) => {
-        const url = `${location.protocol}//${location.host}/manifest.json`;
-        return fetch(url, { signal })
-            .then(res => res.json())
-            .then(() => {
-                return Date.now();
-            });
+const waitFor = (delayed) =>
+    new Promise((resolve) => {
+        setTimeout(resolve, delayed);
+    });
+
+// cache to local storage
+const cache = createLocalStorageCache('@fetcher');
+
+// resolves with current timestamp
+const timestampFetcher = createFetcher(
+    () => {
+        return waitFor(1000).then(() => {
+            return Date.now();
+        });
     },
     {
-        cache: createLocalStorageCache('@fetcher'),
-        cacheKeyPrefix: 'manifest',
+        cache,
+        cacheKeyPrefix: 'timestamp',
         cacheMode: CacheMode.Default,
         cacheMaxAge: 10,
         cacheMinFresh: 3,
     },
 );
 
-function DemoBoard({ data, buttons }) {
+function App() {
+    const { data: swr, refresh } = useSWR(timestampFetcher, null, { retryOnError: true });
+    const { data: polling, start, stop } = usePolling(timestampFetcher, 2);
+
     return (
-        <div>
-            <div>{data}</div>
-            {buttons.map((item, index) => {
-                return (
-                    <button key={index} onClick={item.onClick}>
-                        {item.title}
-                    </button>
-                );
-            })}
-        </div>
+        <table>
+            <tbody>
+                <DemoBoard
+                    title={'useSWR()'}
+                    data={swr}
+                    buttons={[{ title: 'refresh', onClick: refresh }]}
+                ></DemoBoard>
+                <DemoBoard
+                    title={'usePolling()'}
+                    data={polling}
+                    buttons={[
+                        { title: 'start', onClick: start },
+                        { title: 'stop', onClick: stop },
+                    ]}
+                ></DemoBoard>
+            </tbody>
+        </table>
     );
 }
 
-function App() {
-    const { data: swr } = useSWR(fetcher, null, { retryOnError: true });
-    const { data: polling, start, stop } = usePolling(fetcher, 2);
-
+function DemoBoard({ title, data, buttons }) {
     return (
-        <div className="App">
-            <DemoBoard data={swr} buttons={[]}></DemoBoard>
-            <DemoBoard
-                data={polling}
-                buttons={[
-                    { title: 'start', onClick: start },
-                    { title: 'stop', onClick: stop },
-                ]}
-            ></DemoBoard>
-        </div>
+        <tr>
+            <td>{title}</td>
+            <td>{data ?? 'no data'}</td>
+            <td>
+                {buttons.map((item, index) => {
+                    return (
+                        <button key={index} onClick={item.onClick}>
+                            {item.title}
+                        </button>
+                    );
+                })}
+            </td>
+        </tr>
     );
 }
 
