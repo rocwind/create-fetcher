aa4307933eb7152fa789d0c15f7a06b178be0598
Roc Wu <rocwind@gmail.com>
2021-02-23 11:06:54 +0800

    fix(storage)!: switch to @react-native-async-storage/async-storage package for async-storage

--- src/caches/async-storage.ts
+++ src/caches/async-storage.ts
@@ -1,4 +1,4 @@
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { Cache } from '../types';
 import { withMemoryCache } from './memory';
 import { KeyPrefixHelper, defaultPrefix } from './utils';
