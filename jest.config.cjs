efa848ce40303d77c61c3516b33c0408003fbe66
Roc Wu <rocwind@gmail.com>
2021-03-25 21:15:09 +0800

    chore: update ci config and editorconfig

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,15 +1,8 @@
 module.exports = {
-  "roots": [
-    "src"
-  ],
-  "transform": {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  "moduleFileExtensions": [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-  ],
-}
+    roots: ['src'],
+    transform: {
+        '^.+\\.tsx?$': 'ts-jest',
+    },
+    testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+};
